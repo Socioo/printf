51d1f3264fcd8dd5492e7b1b4623d25a9dc7716c
Ahmad Badamasi <31241487+Socioo@users.noreply.github.com>
2022-10-21 16:54:29 +0000

    Update print_custom.c

--- print_custom.c
+++ print_custom.c
@@ -90,8 +90,8 @@ int print_rot13(va_list l, flags_t *f)
 		{
 			for (i = 0; i <= 52; i++)
 			{
-				if (s[j] == rot13[i])
-					_putchar(ROT13[i]);
+				if (s[j] == (ROT13[i])
+					_putchar(rot13[i]);
 			}
 		}
 	}
